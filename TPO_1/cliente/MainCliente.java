91c6eca9e18aacb0d50cb5ff1fd11b67c2e76a40
Cristian <borgesvive@hotmail.com>
2020-03-17 15:41:33 -0300

    Corrige problema de espacios

--- TPO_1/cliente/MainCliente.java
+++ TPO_1/cliente/MainCliente.java
@@ -7,12 +7,12 @@ public class MainCliente {
 
     private final static int PORT = 10578;
     private final static String SERVER = "localhost";
-    
-    public static void main(String[] args) {
-    	boolean exit = false;	// bandera para controlar ciclo del programa
-        Socket socket;			// socket para la comunicacion cliente servidor
 
-        try {            
+    public static void main(String[] args) {
+        boolean exit = false;   // bandera para controlar ciclo del programa
+        Socket socket;          // socket para la comunicacion cliente servidor
+
+        try {
             System.out.println("Cliente> Inicio");
             while( !exit ) {
                 socket = new Socket(SERVER, PORT);  // abre socket
@@ -29,14 +29,14 @@ public class MainCliente {
 
                 if (request.equals("exit")) {
                     exit = true;
-					System.out.println("Cliente> Fin de programa");
+                    System.out.println("Cliente> Fin de programa");
                 } else {
-					// manda peticion al servidor
-					output.writeUTF(request);
-					// captura respuesta e imprime
-					String st = input.readUTF();
-					if( st != null ) System.out.println("Servidor> " + st );
-				}
+                    // manda peticion al servidor
+                    output.writeUTF(request);
+                    // captura respuesta e imprime
+                    String st = input.readUTF();
+                    if( st != null ) System.out.println("Servidor> " + st );
+                }
 
                 output.close();
                 socket.close();
