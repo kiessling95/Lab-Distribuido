b76aaaffd4814fef27a898411a6437b4c1b8a505
german <gkiessling_95@hotmail.com>
2020-03-30 22:59:38 -0300

    Horoscopo.txt

--- TPO_1/src/server/ServidorHoroscopo.java
+++ TPO_1/src/server/ServidorHoroscopo.java
@@ -53,43 +53,48 @@ public class ServidorHoroscopo {
      */
     public static String process(String request) {
         String result = "";
-
-        //frases
-        String[] phrases = {
-        "Jornada propicia para romper la rutina. Sorprende a tu pareja con un presente, o una salida sorpresa juntos.",
-        "No hagas promesas de amor que no estas dispuesto a cumplir fielmente. No juegues con los sentimientos de los demas.",
-        "Si necesitas de una relacion mucho mas intensa debes decirlo de forma clara. No puedes vivir sin cumplir tus deseos.",
-        "Recibiras el toque magico del afecto. Aprovecha el poder estar con quien deseas en el momento indicado.",
-        "Procura dejar la presión de lado en la pareja. Otorga a tu pareja los tiempos que necesite para alcanzar la confianza.",
-        "Tu pareja podria ser pura conversacion y nada de accion. Presta atencion a los detalles de la relacion como cumpleanios y fechas importantes.",
-        "Iniciaras la jornada de hoy con muy buen animo en pareja. Mantenlo durante el dia mediante llamadas o mensajes a ella.",
-        "Sufriras una serie de malentendidos con la familia de tu pareja que pondra cierta tension en el aire. No temas hablarlo.",
-        "Recibiras un sin fin de propuestas sentimentales. En principio querras aceptarlas a todas, pero deberas dejar algunas.",
-        "Las relaciones amorosas pasan por un buen momento, aparecen nuevas e interesantes personas que te motivan a conseguir ideales.",
-        "Nuevas sensaciones te invadiran al encontrarte con un viejo amor del pasado. Jornada propicia para cenas romanticas.",
-        "No debes caer presa de la presion que pueda ejercer tu entorno acerca de tus sentimientos hacia tu pareja. Se tu mismo.",
-        "Vas a superar esto. No durara para siempre. Lo que vives en este momento es temporal. El dolor que sientes, en unos meses se convertira solo en un recuerdo.",
-        "Es verdad, todo apesta en este momento, pero te sorprenderas las grandes cosas que te estan esperando. Todo lo que has soniado esta a tu alcance. Continua avanzando hacia esa vida de ensuenio que deseas.",
-        "La mejor venganza es aprender a amarte a ti misma. No te gastes por demostrarlo a los demas. Toma la vida lo mejor posible, no importa si te lo reconocen o no los demas!",
-        "Mereces ser feliz, nunca permitas que te digan lo contrario. Tu corazon está lleno de bondad y eso es muy dificil de encontrar. Somos afortunados quienes podemos compartir una tarde contigo, lo mejor seria que tu nos mantengas cerca el resto de tu vida.",
-        "Veras que todo tu esfuerzo sera recompensado. Estas trabajando muy duro para alcanzar tus metas; aprende a ser paciente. Llegara lo que tanto anhelas",
-        "Perder una batalla, no significa perder la guerra. No permitas que ningun fracaso te aleje de tus suenios, Un mal dia no significa que toda tu vida apesta. Si te rompen el corazon, no es sinonimo de renunciar al amor",
-        "Eres hermosa tanto por dentro como por fuera; mereces una relacion sana llena de amor. Si alguien te hace dudar de ello, no vale la pena que desperdicies ni un segundo de tu tiempo en el",
-        "Has evolucionado, ya no eres la misma persona que eras ayer. Lograste florecer, asi que deja de preocuparte por tus errores del pasado y enfocate en construir tu futuro",
-        "Aprende a amarte, deja de mirarte de esa forma poco amorosa. Ni eres una carga para los demas, ni estas llena de tantos defectos como lo piensas. Quierete mucho",
-        "Deja la frustracion a un lado, mira todo lo que has avanzado rumbo a tus metas! Sientete orgullosa de tus logros y deja de criticarte tanto. Lo estas haciendo perfecto",
-        "Tu fuerza interior es superior a lo que te imaginas. Mirate desde otra perspectiva para que puedas apreciar tus logros, has realizado todo lo que una vez soniaste!",
-        "Sal a buscar tus suenios, persigue tu felicidad, yo estoy aqui para apoyarte en todo momento, no tienes por que sentirte sola, porque voy sosteniendo tu mano, acompaniandote en todo el camino"
-        };
-
-        ArrayList<String> phrasesList = new ArrayList<String>();
-        Collections.addAll(phrasesList, phrases);
-
-        Collections.shuffle(phrasesList);
-
-        result = phrasesList.get(0);
+        File archivo = null;
+        FileReader fr = null;
+        BufferedReader br = null;
 
         try {
+            try {
+                archivo = new File ("Horoscopo.txt");
+                System.out.println(archivo.getAbsolutePath());
+                fr = new FileReader(archivo);
+                br = new BufferedReader(fr);
+                
+                // Lectura del fichero
+
+                String linea;
+                ArrayList<String> phrasesList = new ArrayList<String>();
+                while((linea=br.readLine())!=null){
+                    phrasesList.add(linea);
+                }
+                Collections.addAll(phrasesList);
+
+                Collections.shuffle(phrasesList);
+
+                result = phrasesList.get(0);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                //TODO: handle exception
+            }finally{
+                // En el finally cerramos el fichero, para asegurarnos
+                // que se cierra tanto si todo va bien como si salta 
+                // una excepcion.
+                try{                    
+                   if( null != fr ){   
+                      fr.close();     
+                   }                  
+                }catch (Exception e2){ 
+                   e2.printStackTrace();
+                }
+             }
+            
+            
+            
             Thread.sleep(5000);
         } catch (InterruptedException ex) {
             System.err.println(ex.getMessage());
