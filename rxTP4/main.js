15001793b250e7203b9309f9d2a3f080789a76c4
german <gkiessling_95@hotmail.com>
2020-06-16 13:46:47 -0300

    prueba chat rx
    typing en desarrollo

---
+++ rxTP4/main.js
@@ -0,0 +1,97 @@
+// Cuando se termina de cargar la pagina, se inicializa todo
+window.addEventListener("load", function (event) {
+  var socket = io(); // Socket.io
+
+  // Al recibir un mensaje, crea un elemento en la lista chat mostrando dicho mensaje
+  socket.on("message", function (msg) {
+    
+    var liMessageRecibed=document.createElement('li');
+    liMessageRecibed.className="messageReceived";
+
+    var divFrom=document.createElement('div');
+    divFrom.className="formatFrom";
+
+    var divMessage=document.createElement('div');
+    divMessage.className="formatMessage";
+
+    var textMessage=document.createTextNode(msg.message);
+    var textFrom=document.createTextNode(" "+msg.from+":");
+
+    divFrom.innerHTML=('<i class="fas fa-user-alt"></i>')
+    divFrom.appendChild(textFrom);
+    divMessage.appendChild(textMessage)
+
+    liMessageRecibed.append(divFrom);
+    liMessageRecibed.append(divMessage);
+
+    $("#chat").append(liMessageRecibed);
+
+   
+  });
+
+  // Cuando se presiona el boton de 'Send', se envia el mensaje
+  $("#sendButton").on("click", function () {
+    sendMessage();
+  });
+
+  // Cuando se apreta 'enter', se envia el mensaje
+  $("#messageToSend").keypress(function (e) {
+    if (e.which == 13) {
+      sendMessage();
+    }
+  });
+
+  // Cuando se apreta 'enter', se asigna el nuevo nick
+  $("#nickname").keypress(function (e) {
+    if (e.which == 13) {
+      var to = $("#nickActual").text();
+      if (to == "Nick:") {
+        var name = $("#nickname")
+          .val()
+          .trim();
+        if (!name) {
+          return false;
+        }
+        socket.emit("name", name);
+        $("#nickname")
+          .val("");
+        $("#nickActual").text(name);
+      }
+    }
+  });
+
+  // Funcion utilizada para obtener el mensaje escrito y enviarlo al servidor
+  function sendMessage() {
+    var message = $("#messageToSend")
+      .val()
+      .trim();
+    if (!message) {
+      return false;
+    }
+    var name = $("#nicknameDestinatario")
+      .val()
+      .trim();
+    socket.emit("messageTo", { "message": message, "to": name });
+    $("#messageToSend")
+      .val("")
+      .focus();
+    $("#chat").append($('<li class="messageSended">').text(message)); 
+    // Agrega el mensaje a la lista del chat como un mensaje enviado por el cliente, y no recibido del servidor
+   
+   
+    $("#contenedor_chat").stop().animate({ scrollTop: $("#contenedor_chat")[0].scrollHeight }, 1000);
+
+    //isTyping event
+    $("#messageToSend").keypress(function () {
+      socket.emit("typing", { user: "Someone", message: "is typing..." })
+    });
+
+    return false;
+  }
+
+
+
+});
+
+
+
---
+++ rxTP4/server.js
@@ -0,0 +1,56 @@
+var express = require("express"); // Import del modulo express.js
+var app = express();
+var server = app.listen(4000, function(){
+  console.log('server is running on port', server.address().port);
+});
+var io = require("socket.io")(server); // Import del modulo socket.io
+
+
+var nickname = new Map(); // Nickname -> SocketID
+var clientes = new Map(); // SocketID -> Nickname 
+
+
+
+app.use(express.static(__dirname +'/')); // Se establece el path de los archivos estaticos (css, imagenes, etc.)
+
+// Cuando se accede a la pagina envia el html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// Cuando hay una conexion...
+io.on("connection", function (socket) {
+
+  socket.on("name", function (name) {
+    nickname.set(name, socket.id);
+    clientes.set(socket.id, name);
+    console.log("Se conecto " + name);
+  });
+
+  // Al recibir un mensaje, hacer...
+  socket.on("messageTo", function (msg) {
+    // Enviar mensaje a todos los nombres menos al emisor
+    console.log(msg);
+    var destinatario = nickname.get(msg.to);
+    var emisor = clientes.get(socket.id);
+    io.to(destinatario).emit('message', { "message": msg.message, "from": emisor });
+  });
+
+  socket.on("disconnect", function () {
+
+    var userName=clientes.get(socket.id);
+    console.log(userName+' Cerro la sesion');
+    socket.broadcast.emit("disconnect");
+    nickname.delete(clientes.get(socket.id))
+    clientes.delete(socket.id);
+  });
+
+});
+
+// Inicia el servidor http en el puerto designado
+//http.listen(port, function () {
+//  console.log("Server started on: " + port);
+//});
+
+ // sending to individual socketid (private message)
+ //io.to('${socketId}').emit('hey', 'I just met you');
